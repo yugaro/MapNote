22a134443ce34ae863daa794fba219f14f26a078
yugaro <yugaro.yugaro@gmail.com>
2021-11-30 15:28:40 +0900

    Initial Commit

--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import { View, Dimensions, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View, Dimensions, StyleSheet, Alert,
+} from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function MapScreen() {
+  const [location, setLocation] = useState({
+    latitude: 35.681236,
+    longitude: 139.767125,
+  });
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.02,
+          longitudeDelta: 0.02,
         }}
-      />
-      <Marker
-        coordinate={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+        onRegionChange={(region) => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
         }}
-      />
+      >
+        <Marker
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+          title="This is a marker"
+          description="This is a marker example."
+        />
+      </MapView>
     </View>
   );
 }
@@ -33,6 +48,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
+    flex: 1,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
