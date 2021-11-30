34136872d9c288e8af25c3e9d1e154286ab93843
yugaro <yugaro.yugaro@gmail.com>
2021-11-30 11:13:30 +0900

    Initial Commit

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { CardStyleInterpolators } from '@react-navigation/stack';
 import FirstScreen from './src/screens/FirstScreen';
 import SecondScreen from './src/screens/SecondScreen';
 import ThirdScreen from './src/screens/ThirdScreen';
@@ -10,7 +11,16 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Second">
+      <Stack.Navigator
+        initialRouteName="Second"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#fdafde' },
+          headerTitleStyle: { color: '#ffffff' },
+          headerTintColor: '#ffffff',
+          headerBackTitle: 'Back',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
         <Stack.Screen name="First" component={FirstScreen} />
         <Stack.Screen name="Second" component={SecondScreen} />
         <Stack.Screen name="Third" component={ThirdScreen} />
